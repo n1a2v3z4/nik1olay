3f214de791e10e9856ecf2b51060f422d150e31a
nik1olay <85620548+nik1olay@users.noreply.github.com>
2021-07-19 21:41:01 +0300

    Initial Commit

---
+++ proekt2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proekt2
+//
+//  Created by Cергей Иванович on 19.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
